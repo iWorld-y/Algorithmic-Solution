4bfa61a56b7914792baf4154ddcd52a3be149442
Eugene <caima12138@foxmail.com>
2022-08-19 20:41:46 +0800

    103. 二叉树的锯齿形层序遍历

--- DataStruct/LeetCode/103.二叉树的锯齿形层序遍历.cpp
+++ DataStruct/LeetCode/103.二叉树的锯齿形层序遍历.cpp
@@ -47,33 +47,45 @@ struct TreeNode {
         : val(x), left(left), right(right) {}
 };
 
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
    public:
-    /*
-     * 低效的二重循环
-     */
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.max_size(); i++) {
-            for (int j = i; j < nums.max_size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+    vector<TreeNode*> v1, v2;
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (root == nullptr) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        int cnt = 1;
+        while (!q.empty()) {
+            ans.push_back(vector<int>());
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                ans.back().push_back(node->val);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            if (cnt++ % 2 == 0) {
+                reverse(ans.back().begin(), ans.back().end());
             }
         }
-    }
-    /*
-     * 哈希表
-     */
-    vector<int> twoSum_2(vector<int>& nums, int target) {
-        unordered_map<int, int> hashTable;
-        for (int i = 0; i < nums.size(); i++) {
-            auto it = hashTable.find(target - nums[i]);
-            if (it != hashTable.end()) {
-                return {it->second, i};
-            }
-            hashTable[nums[i]] = i;
-        }
-        return {};
+        return ans;
     }
 };
+// @lc code=end
+
 int main(int argc, char* argv[]) { return 0; }
