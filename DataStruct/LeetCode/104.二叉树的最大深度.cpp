3fa75e843bc18441d0174640cdf39be601abef5c
Eugene <caima12138@foxmail.com>
2022-08-22 14:04:32 +0800

    104. 二叉树的最大深度

---
+++ DataStruct/LeetCode/104.二叉树的最大深度.cpp
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+// @lc app=leetcode.cn id=94 lang=cpp
+
+const int MAXN = 0xffff;
+const int INF = 0x3f3f3f3f;
+
+typedef long long ll;
+typedef double fl;
+
+#define mem(arr, num) memset(arr, num, sizeof(arr));
+#define random(a, b) ((a) + rand() % ((b) - (a) + 1))
+#define elif else if
+#define max(a, b) (a > b ? a : b)
+#define min(a, b) (a < b ? a : b)
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        queue<TreeNode*> q;
+        int level = 0;
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            while (size > 0) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+                size--;
+            }
+            level++;
+        }
+        return level++;
+    }
+    // int maxDepth
+};
+// @lc code=end
+
+int main(int argc, char* argv[]) { return 0; }
--- DataStruct/LeetCode/tempCodeRunner.cpp
+++ DataStruct/LeetCode/tempCodeRunner.cpp
@@ -1,69 +1,61 @@
-#define _CRT_SECURE_NO_DEPRECATE
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <functional>
 #include <iostream>
-#include <map>
 #include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <vector>
+#include "tree.h"
 
 using namespace std;
+const int maxSize = 100;
 
-// @lc app=leetcode.cn id=94 lang=cpp
-
-const int maxprofit = 0xffff;
-const int INF = 0x3f3f3f3f;
-
-typedef long long ll;
-typedef double fl;
-
-#define mem(arr, num) memset(arr, num, sizeof(arr));
-#define random(a, b) ((a) + rand() % ((b) - (a) + 1))
-#define elif else if
-#define max(a, b) (a > b ? a : b)
-#define min(a, b) (a < b ? a : b)
-
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right)
-        : val(x), left(left), right(right) {}
-};
-
-// @lc code=start
-class Solution {
-   public:
-    int maxProfit(vector<int>& prices) {
-        int maxprofit = 0, minprice = INT32_MAX;
-        for (int i = 0; i < prices.size(); i++) {
-            maxprofit = max(maxprofit, prices[i] - minprice);
-            minprice = min(minprice, prices[i]);
+int Btdepth(TreeNode *root) {
+    if (!root)
+        return 0;
+    TreeNode *q[maxSize];
+    int front = 0, rear = 0, level = 0;
+    q[rear++] = root;
+    while (front < rear) {
+        // 存储当前层次的区间
+        int size = rear - front;
+        // 若当前层次没有遍历完全则继续
+        while (size > 0) {
+            TreeNode *node = q[front++];
+            if (node->left)
+                q[rear++] = node->left;
+            if (node->right)
+                q[rear++] = node->right;
+            size--;
         }
-        return maxprofit;
+        level++;
     }
-};
-// @lc code=end
+    return level;
+}
+//int Btdepth(TreeNode *root) {
+//    if (root == nullptr) return 0;
+//    queue<TreeNode *> Q;
+//    Q.push(root);
+//    int ans = 0;
+//    while (!Q.empty()) {
+//        int sz = Q.size();
+//        while (sz > 0) {
+//            TreeNode *node = Q.front();
+//            Q.pop();
+//            if (node->left) Q.push(node->left);
+//            if (node->right) Q.push(node->right);
+//            sz -= 1;
+//        }
+//        ans += 1;
+//    }
+//    return ans;
+//}
 
-int main(int argc, char* argv[]) {
-    vector<int> arr = {7, 1, 5, 3, 6, 4};
-    Solution s;
-    cout << s.maxProfit(arr);
+int main() {
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    root->right->right = new TreeNode(6);
+
+    cout << Btdepth(root);
     return 0;
 }
