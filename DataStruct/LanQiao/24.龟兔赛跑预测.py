9810acd2338048203bb69deeee31bc29c95edce2
iWorld Jiang <caima12138@foxmail.com>
2023-03-01 20:38:38 +0800

    试题 基础练习 龟兔赛跑预测

--- DataStruct/LanQiao/24.龟兔赛跑预测.py
+++ DataStruct/LanQiao/24.龟兔赛跑预测.py
@@ -7,7 +7,32 @@
 
 def main():
     v1, v2, t, s, l = map(int, input().split())
-    
+    cnt_time: int = 0
+    lt: int = 0
+    lr: int = 0
+    is_resting: bool = False
+    break_time: int = 0
+    while (lt < l and lr < l):
+        if (is_resting == False and lr - lt < t or is_resting == True and breaktime >= s):
+            # 若兔子没在休息, 且与乌龟的距离 < t
+            # 或兔子正在休息, 且休息时间够了
+            # 则奔跑
+            lr = lr + v1
+            is_resting = False
+            breaktime = 0
+        else:
+            is_resting = True
+            breaktime += 1
+        lt = lt + v2
+        cnt_time += 1
+
+    if (lt >= l and lr < l):
+        print('T')
+    elif (lt < l and lr >= l):
+        print('R')
+    else:
+        print('D')
+    print(cnt_time)
 
 
 main()
