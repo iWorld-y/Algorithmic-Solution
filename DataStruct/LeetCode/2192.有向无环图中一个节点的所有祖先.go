f37ecc844d589b2be0f52d126ea19c0f5f732858
iWorld-y <caima12138@foxmail.com>
2024-06-15 21:53:50 +0800

    new: 2192.有向无环图中一个节点的所有祖先.go

--- DataStruct/LeetCode/2192.有向无环图中一个节点的所有祖先.go
+++ DataStruct/LeetCode/2192.有向无环图中一个节点的所有祖先.go
@@ -14,6 +14,8 @@ func getAncestors(n int, edges [][]int) [][]int {
 	for _, edge := range edges {
 		in[edge[1]] = append(in[edge[1]], edge[0])
 	}
+
+	// i: curr 的某个祖先
 	var dfs func(curr, i int, visited []bool)
 	dfs = func(curr, i int, visited []bool) {
 		for _, next := range in[i] {
@@ -27,25 +29,7 @@ func getAncestors(n int, edges [][]int) [][]int {
 	}
 	for i := 0; i < n; i++ {
 		dfs(i, i, make([]bool, n))
-		ans[i] = CleanInts(ans[i])
+		sort.Ints(ans[i])
 	}
 	return ans
-}
-
-func UniqueInt(arr []int) []int {
-	m := make(map[int]int, len(arr))
-	ans := make([]int, 0, len(arr))
-	for _, i := range arr {
-		m[i]++
-		if m[i] == 1 {
-			ans = append(ans, i)
-		}
-	}
-	return ans
-}
-
-func CleanInts(arr []int) []int {
-	arr = UniqueInt(arr)
-	sort.Ints(arr)
-	return arr
 }
