01edf1dfe8d875533181f8dc0c7ec76616b7761f
iWorld-y <caima12138@foxmail.com>
2024-06-23 17:50:23 +0800

    2192. 有向无环图中一个节点的所有祖先

--- DataStruct/LeetCode/2192.有向无环图中一个节点的所有祖先.go
+++ DataStruct/LeetCode/2192.有向无环图中一个节点的所有祖先.go
@@ -1,32 +1,29 @@
 package LeetCode
 
-import (
-	"sort"
-)
+import "sort"
 
-func GetAncestors(n int, edges [][]int) [][]int {
-	return getAncestors(n, edges)
-}
 func getAncestors(n int, edges [][]int) [][]int {
-	in := make([][]int, n)
+	inMap := make(map[int][]int)
 	ans := make([][]int, n)
-	// 入度
 	for _, edge := range edges {
-		in[edge[1]] = append(in[edge[1]], edge[0])
+		inMap[edge[1]] = append(inMap[edge[1]], edge[0])
 	}
 
-	// i: curr 的某个祖先
 	var dfs func(curr, i int, visited []bool)
 	dfs = func(curr, i int, visited []bool) {
-		for _, next := range in[i] {
+		if len(inMap[i]) == 0 {
+			return
+		}
+		for _, next := range inMap[i] {
 			if visited[next] {
 				continue
 			}
-			visited[next] = true
 			ans[curr] = append(ans[curr], next)
+			visited[next] = true
 			dfs(curr, next, visited)
 		}
 	}
+
 	for i := 0; i < n; i++ {
 		dfs(i, i, make([]bool, n))
 		sort.Ints(ans[i])
--- DataStruct/LeetCode/2192.有向无环图中一个节点的所有祖先_test.go
+++ DataStruct/LeetCode/2192.有向无环图中一个节点的所有祖先_test.go
@@ -1,11 +1,11 @@
-package LeetCode_test
+package LeetCode
 
 import (
-	"github.com/iWorld-y/Algorithmic-Solution/DataStruct/LeetCode"
 	"testing"
 )
 
 func TestGetAncestors(t *testing.T) {
-	t.Log(LeetCode.GetAncestors(8, [][]int{{0, 3}, {0, 4}, {1, 3}, {2, 4}, {2, 7}, {3, 5}, {3, 6}, {3, 7}, {4, 6}}))
-	t.Log(LeetCode.GetAncestors(9, [][]int{{3, 6}, {2, 4}, {8, 6}, {7, 4}, {1, 4}, {2, 1}, {7, 2}, {0, 4}, {5, 0}, {4, 6}, {3, 2}, {5, 6}, {1, 6}}))
+	t.Log(getAncestors(8, [][]int{{0, 3}, {0, 4}, {1, 3}, {2, 4}, {2, 7}, {3, 5}, {3, 6}, {3, 7}, {4, 6}}))
+	t.Log(getAncestors(5, [][]int{{0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}}))
+	t.Log(getAncestors(9, [][]int{{3, 6}, {2, 4}, {8, 6}, {7, 4}, {1, 4}, {2, 1}, {7, 2}, {0, 4}, {5, 0}, {4, 6}, {3, 2}, {5, 6}, {1, 6}}))
 }
