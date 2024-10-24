e1e7c152b280771d3195c722f1d7feef9a17b631
iWorld-y <caima12138@foxmail.com>
2024-10-24 23:09:10 +0800

    239. 滑动窗口最大值.go

--- DataStruct/LeetCode/Golang/239. 滑动窗口最大值.go
+++ DataStruct/LeetCode/Golang/239. 滑动窗口最大值.go
@@ -1,47 +1,25 @@
 package LeetCode
 
-import (
-	"container/heap"
-	"sort"
-)
-
-var a []int
-
-type hp struct {
-	sort.IntSlice
-}
-
-func (h *hp) Less(i, j int) bool {
-	return a[h.IntSlice[i]] > a[h.IntSlice[j]]
-}
-
-func (h *hp) Push(value any) {
-	h.IntSlice = append(h.IntSlice, value.(int))
-}
-
-func (h *hp) Pop() any {
-	length := len(h.IntSlice)
-	v := h.IntSlice[length-1]
-	h.IntSlice = h.IntSlice[:length-1]
-	return v
-}
-
 func maxSlidingWindow(nums []int, k int) []int {
-	a = nums
-	queue := &hp{make([]int, k)}
-	for i := 0; i < k; i++ {
-		queue.IntSlice[i] = i
-	}
-	heap.Init(queue)
-	length := len(nums)
-	ans := make([]int, 1, length-k+1)
-	ans[0] = nums[queue.IntSlice[0]]
-	for i := k; i < len(nums); i++ {
-		heap.Push(queue, i)
-		for queue.IntSlice[0] <= i-k {
-			heap.Pop(queue)
+	var q []int
+	push := func(i int) {
+		for len(q) > 0 && nums[i] >= nums[q[len(q)-1]] {
+			q = q[:len(q)-1]
 		}
-		ans = append(ans, nums[queue.IntSlice[0]])
+		q = append(q, i)
+	}
+	for i := 0; i < k; i++ {
+		push(i)
+	}
+	n := len(nums)
+	ans := make([]int, 0, n-k+1)
+	ans = append(ans, nums[q[0]])
+	for i := k; i < n; i++ {
+		push(i)
+		for q[0] <= i-k {
+			q = q[1:]
+		}
+		ans = append(ans, nums[q[0]])
 	}
 	return ans
 }
